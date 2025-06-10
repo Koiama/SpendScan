1312d206fec997d7d7148c10c942f81e2e6d09a2
Koiama <groznaaksenia@example.com>
2025-06-10 18:55:03 +0300

    topbar

--- app/src/main/java/com/spendscan/ExpensesScreen.kt
+++ app/src/main/java/com/spendscan/ExpensesScreen.kt
@@ -1,18 +1,46 @@
 package com.spendscan
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ExpensesScreen(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        Text(
-            text = "Экран расходы",
-            fontSize = 30.sp
-        )
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary
+                ) ,
+                title = {
+                    Text(
+                        "Расходы сегодня"
+                    )
+                }
+            )
+
+        },
+        floatingActionButton = {
+
+        }
+    ) { innerPadding ->
+        Column(modifier = modifier.padding(innerPadding)) {
+            Text(
+                text = "Экран расходы",
+                fontSize = 30.sp
+            )
+        }
     }
 }
 
