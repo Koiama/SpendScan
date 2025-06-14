bd08017923008e3eecbe71e4a04acbf0160d23ee
Koiama <groznaaksenia@example.com>
2025-06-14 21:39:50 +0300

    Deleted button shadow

--- app/src/main/java/com/spendscan/ui/components/FloatingAddButton.kt
+++ app/src/main/java/com/spendscan/ui/components/FloatingAddButton.kt
@@ -1,19 +1,26 @@
 package com.spendscan.ui.components
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import com.spendscan.R
@@ -24,21 +31,25 @@ fun FloatingAddButton(
     modifier: Modifier = Modifier
 ) {
 
-    Box(modifier = Modifier.fillMaxSize()) {
-        FloatingActionButton(
-            onClick =onClick,
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp, 14.dp),
+        contentAlignment = Alignment.BottomEnd
+    ) {
+        OutlinedButton(
+            onClick = onClick,
             shape = CircleShape,
-            elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation(),
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(bottom = 14.dp, end = 16.dp)
-                .size(56.dp)
+            border = BorderStroke(0.dp, MaterialTheme.colorScheme.primary),
+            contentPadding = PaddingValues(0.dp),
+            modifier = Modifier.size(56.dp),
+            colors = ButtonDefaults.outlinedButtonColors(
+                contentColor = MaterialTheme.colorScheme.onSurface
+            )
         ) {
-            Icon(
+            Image(
                 imageVector = ImageVector.vectorResource(id = R.drawable.button_circle),
-                contentDescription = "Добавить доход",
-                tint = Color.Unspecified,
-                modifier = Modifier.size(56.dp)
+                contentDescription = "Добавить доход"
             )
         }
     }
